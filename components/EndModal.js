ce40adc0df089581d863effe9351d80b4ce68856
Chris Minnick <chris@minnick.com>
2018-05-02 18:00:33 -0700

    making new and better version

---
+++ components/EndModal.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {
+    Alert, AsyncStorage, AppRegistry, Button, Image, Platform, Share, StyleSheet, Text, TextInput, TouchableHighlight,
+    TouchableOpacity, View, Linking
+} from 'react-native';
+import Modal from 'react-native-modal';
+
+import {styles} from "./ConfigModal.style";
+
+class EndModal extends Component {
+
+    render(){
+        return(
+            <Modal
+                backdropColor={"black"}
+                backdropOpacity={1}
+                animationIn={"slideInDown"}
+                animationOut={"slideOutUp"}
+                animationInTiming={300}
+                animationOutTiming={300}
+                backdropTransitionInTiming={1000}
+                backdropTransitionOutTiming={1000}
+                avoidKeyboard={true}
+                isVisible={this.props.modalVisible}
+            >
+                <View style={styles.modalContent}>
+
+                    <Text style={styles.modalHeading}>Welcome Back!</Text>
+                    <Image source={require('../images/animated-pirate-image-0043.gif')} />
+                    <Text style={styles.bodytext}>Ahoy! How many words ya got in the bank now?</Text>
+                    <TextInput
+                        style={styles.configInput}
+                        autoFocus ="true"
+                        keyboardType="numeric"
+                        placeholder="Total Words"
+                        value={this.props.totalWords}
+                        onChangeText={(value) => this.props.setTotal(value)}
+                    />
+                    <TouchableOpacity onPress={() => this.props.closeModal()}>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>Save</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </Modal>
+        )
+    }
+}
+
+export default EndModal;
---
+++ components/StartModal.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {
+    Alert, AsyncStorage, AppRegistry, Button, Image, Platform, Share, StyleSheet, Text, TextInput, TouchableHighlight,
+    TouchableOpacity, View, Linking
+} from 'react-native';
+import Modal from 'react-native-modal';
+
+import {styles} from "./ConfigModal.style";
+
+class StartModal extends Component {
+
+    render(){
+        return(
+            <Modal
+                backdropColor={"black"}
+                backdropOpacity={1}
+                animationIn={"slideInDown"}
+                animationOut={"slideOutUp"}
+                animationInTiming={300}
+                animationOutTiming={300}
+                backdropTransitionInTiming={1000}
+                backdropTransitionOutTiming={1000}
+                avoidKeyboard={true}
+                isVisible={this.props.modalVisible}
+            >
+            <View style={styles.modalContent}>
+
+                <Text style={styles.modalHeading}>Start a Sprint</Text>
+                <Image source={require('../images/animated-pirate-image-0043.gif')} />
+                <Text style={styles.bodytext}>Ahoy! How many words be ya starting this sprint with in the bank?</Text>
+                <TextInput
+                    style={styles.configInput}
+                    autoFocus ="true"
+                    keyboardType="numeric"
+                    placeholder="Total Words"
+                    value={this.props.totalWords}
+                    onChangeText={(value) => this.props.setTotal(value)}
+                />
+                <TouchableOpacity onPress={() => this.props.closeModal()}>
+                    <View style={styles.button}>
+                        <Text style={styles.buttonText}>Save</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+            </Modal>
+        )
+    }
+}
+
+export default StartModal;
